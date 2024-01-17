4bce9c33012a046328cb5c421d803fcb14395c46
Fhelipe Bruno <fhelipebruno@gmail.com>
2024-01-17 10:11:25 -0300

    Implementando components e props

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header(props){
+    const title = props.title;
+    return(
+        <header>
+            <h1>{title}</h1>
+        </header>
+    );
+}
