9dae19f35cdc8559f6169f3c3b9bb3ed8ec34731
Fhelipe Bruno <fhelipebruno@gmail.com>
2024-01-18 18:28:08 -0300

    Ajuste de CSS na tela de login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Header from './Header';
+import Login from './pages/Login';
+import './global.css';
 
 export default function App() {
   return (
-    <Header
-      title = "Client REST Udemy"
-    />
+    <Login/>
   );
 }
